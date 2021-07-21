f9d70516625b3bb474736c63d8145e17cb1856b2
Felipe Quintero <ffquint@gmail.com>
2021-07-20 21:54:15 -0400

    Deployed CRA. Removed unnecessary files and cleaned up code on App.js. Created Components folder and Login component.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Sauti Africa!</h1>
+        <h3>The premier marketplace for small business owners.</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,1 @@
+// login page will go here
