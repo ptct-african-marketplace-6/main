fb974001d244072d884b123bc97f3ce350dea793
Felipe Quintero <ffquint@gmail.com>
2021-07-21 20:31:20 -0400

    added boilerplate code to login component.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,1 +1,19 @@
 // login page will go here
+
+import React from "react";
+import { useHistory } from "react-router-dom";
+
+const Login = () => {
+
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <form>
+        {/* <input />
+        <input /> */}
+      </form>
+    </div>
+  );
+};
+
+export default Login;
