b08b5003f732e05569b2bbcc6c421330240ff710
Felipe Quintero <ffquint@gmail.com>
2021-07-22 20:31:38 -0400

    added templated code to reducers file for item reducers

--- src/common/reducers/index.js
+++ src/common/reducers/index.js
@@ -1,24 +1,57 @@
-import { FETCH_ITEMS, ADD_ITEM, EDIT_ITEM, DELETE_ITEM, FETCH_ITEMS_OK, FETCH_ITEMS_ERROR } from "../actions/index"
+import { FETCH_ITEMS, ADD_ITEM, EDIT_ITEM, DELETE_ITEM, FETCH_ITEMS_OK } from "../actions/index"
 
 const initialState = {
   items: [],
-  isLoading: true,
+  isLoading: false,
   error: ''
 }
 
 export const itemReducer = (state = initialState, action) => {
   switch (action.type) {
-    case ADD_ITEM:
+    case FETCH_ITEMS_OK:
       return {
-        const newItem = {
-          item_name: action.payload.item_name,
-          location: ,
-          quantity: ,
-          price: ,
-          description: ,
-          user_id: 
-        }
+        ...state,
+        isLoading: true
       }
+    case FETCH_ITEMS:
+      return {
+        ...state,
+        items: action.payload,
+        isLoading: false
+      }
+    case ADD_ITEM:
+      const newItem = {
+        item_name: action.payload.item_name,
+        location: action.payload.location,
+        quantity: action.payload.quantity,
+        price: action.payload.price,
+        description: action.payload.description,
+        user_id: action.payload.user_id
+      }
+      return {
+        ...state,
+        items: [...state.items, newItem]
+      }
+    case EDIT_ITEM:
+      const editItem = {
+        item_name: action.payload.item_name,
+        location: action.payload.location,
+        quantity: action.payload.quantity,
+        price: action.payload.price,
+        description: action.payload.description,
+      }
+      return {
+        
+        // This code may need more work ... this is just a templated version to begin with
+
+        items: [...state.items, editItem]
+      }
+    case DELETE_ITEM:
+      return {
+        items: state.items.filter(item => (action.payload !== item.id))
+      }
+    default:
+      return state;  
   }
 
 }
