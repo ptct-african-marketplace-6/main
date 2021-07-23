9d0e9a436d195a4b30d05270117ee37e32e9bd0a
Diego Fischer <diego@carupi.com>
2021-07-23 19:43:24 -0300

    signup form working, before styling

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,9 @@
 // login page will go here
 
-import React from "react";
-import { useHistory } from "react-router-dom";
+import React from 'react'
+import { useHistory } from 'react-router-dom'
 
 const Login = () => {
-
   return (
     <div>
       <h1>Login Page</h1>
@@ -13,7 +12,37 @@ const Login = () => {
         <input /> */}
       </form>
     </div>
-  );
-};
+  )
+}
 
-export default Login;
+export default Login
+
+//https://drive.google.com/drive/folders/11iiMC9DiRtoqz77CTCeJPpR8zmiALyw0
+
+/* User Object:
+{
+  id: integer
+  username: string
+  password: string 
+  email: string
+  isOwner: boolean
+}
+Item Object:
+{
+  id: integer
+  item_name: string
+  location: string
+  quantity: integer
+  price: float
+  description: string
+  user_id: integer // this references the id in the user table
+} */
+
+/* These are your ENDPOINTS and we list what each will return:
+Users
+[POST] https://team-amazing.herokuapp.com/api/auth/register
+    returns user object
+[POST] https://team-amazing.herokuapp.com/api/auth/login
+    returns token, user object
+Items
+*/
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,9 +1,167 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import * as yup from 'yup'
 
-const SignUp = () => {
+function SignUp(props) {
+  const [disabledButton, setDisabledButton] = useState(true)
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    pass: '',
+    owner: false,
+  })
+
+  const [errors, setErrors] = useState({
+    name: '',
+    email: '',
+    pass: '',
+    owner: '',
+  })
+
+  const formSchema = yup.object().shape({
+    name: yup
+      .string()
+      .required('Pleae include your name.')
+      .min(6, 'Name must be at least 6 characters long'),
+    email: yup
+      .string()
+      .email('Please include your email address.')
+      .required('Must include email address.'),
+    pass: yup
+      .string()
+      .required('Password is Required')
+      .min(6, 'Passwords must be at least 6 characters long.'),
+    owner: yup.boolean(),
+  })
+
+  const setFormErrors = (name, value) => {
+    yup
+      .reach(formSchema, name)
+      .validate(value)
+      .then(() => setErrors({ ...errors, [name]: '' }))
+      .catch((err) => setErrors({ ...errors, [name]: err.errors[0] }))
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    alert(JSON.stringify(formData))
+  }
+
+  const handleChange = (e) => {
+    const { name, type } = e.target
+    const valueToUse = type === 'checkbox' ? 'checked' : 'value'
+    setFormData((prev) => {
+      return {
+        ...formData,
+        [e.target.name]: e.target[valueToUse],
+      }
+    })
+
+    setFormErrors(name, e.target[valueToUse])
+  }
+
+  useEffect(() => {
+    formSchema.isValid(formData).then((valid) => setDisabledButton(!valid))
+  })
+
   return (
-    <div></div>
+    <>
+      <div class='container-fluid col-md-auto'>
+        <div className='App'>
+          <form
+            onSubmit={handleSubmit}
+            className='d-flex flex-column container-fluid col-md-auto'
+          >
+            <div className='row'>
+              <label>
+                Name
+                <input
+                  name='name'
+                  type='text'
+                  value={formData.name}
+                  onChange={handleChange}
+                />
+              </label>
+            </div>
+
+            <div className='row'>
+              <label>
+                Email
+                <input
+                  name='email'
+                  type='text'
+                  value={formData.email}
+                  onChange={handleChange}
+                />
+              </label>
+            </div>
+
+            <div className='row'>
+              <label>
+                Password
+                <input
+                  name='pass'
+                  type='text'
+                  value={formData.pass}
+                  onChange={handleChange}
+                />
+              </label>
+            </div>
+
+            <div className='row'>
+              <label>
+                Owner?
+                <input
+                  name='owner'
+                  type='checkbox'
+                  checked={formData.owner}
+                  onChange={handleChange}
+                />
+              </label>
+            </div>
+
+            <div className='row'>
+              <button disabled={disabledButton}>Submit!</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
   )
 }
 
 export default SignUp
+
+/* https://drive.google.com/drive/folders/11iiMC9DiRtoqz77CTCeJPpR8zmiALyw0
+
+User Object:
+{
+  id: integer
+  username: string
+  password: string 
+  email: string
+  isOwner: boolean
+}
+Item Object:
+{
+  id: integer
+  item_name: string
+  location: string
+  quantity: integer
+  price: float
+  description: string
+  user_id: integer // this references the id in the user table
+} 
+
+These are your ENDPOINTS and we list what each will return:
+Users
+[POST] https://team-amazing.herokuapp.com/api/auth/register
+    returns user object
+[POST] https://team-amazing.herokuapp.com/api/auth/login
+    returns token, user object
+Items
+
+
+POST https://saudi-market-app.herokuapp.com/api/auth/login
+POST https://saudi-market-app.herokuapp.com/api/auth/register
+ */
