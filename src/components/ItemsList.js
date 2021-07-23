56d8c36d5a89db1b694b65beb0b16c941346ba76
Amanda Burtness <amandaburtness@gmail.com>
2021-07-22 19:00:06 -0500

    flushing out components

--- src/components/ItemsList.js
+++ src/components/ItemsList.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Link, useLocation } from 'react-router-dom'
 
-const ItemsList = () => {
+const ItemsList = (props) => {
+const { items } = props
+const location = useLocation()
+
   return (
-    <div></div>
+    <div>
+      {items.map(item => (
+        <div className="item-card" key={item.id}>
+          <Link to={`${location.pathname}/${item.id}`}>
+            <img className="img-fluid" src={item.imageURL} alt={item.name}/>
+            <p>{item.name}</p>
+          </Link>
+          <p>${item.price}</p>
+        </div>
+      ))}
+    </div>
   )
 }
 
