0befe77b4dc1d1a59aeb8b34afe33f18e2a50a28
Felipe Slaughter-Quintero <fslauq@gmail.com>
2021-07-26 22:05:23 -0400

    updated code on Home component. Also switched Vercel deployment to pull from main repo.

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,37 +15,15 @@ const Home = () => {
       <div className="row py-5">
         <div className="col-12 d-flex justify-content-center align-items-center py-5 my-5">
         <a onClick={routeToShop } className="btn mx-auto">Shop Now!</a>
-
-        <p>This is the premier marketplace for new and established small business owners to expand their presence online.</p>
-
-        <p>We specialize in helping small business owners, especially those who are underrepresented, with the ability to list and sell items online. This allows business owners to reach a wider audience while still remaining in control of their own business.</p>
         </div>
+        {/* <div> */}
+          {/* <p>This is the premier marketplace for new and established small business owners to expand their presence online.</p>
+
+          <p>We specialize in helping small business owners, especially those who are underrepresented, with the ability to list and sell items online. This allows business owners to reach a wider audience while still remaining in control of their own business.</p>
+        </div> */}
       </div>
     </div>
   )
 }
 
-export default Home
-
-// Alternate styling option using react-strap
-
-{/* <div className="home-wrapper">
-<div className='App'><br/>
-<Container>
-  <Card bg="light" >
-    <Card.Header><h3>Welcome to Sauti Market!</h3></Card.Header>
-    <div>
-      <Card.Body>
-
-        This is the premier marketplace for new and established small business owners to expand their presence online.
-      
-      </Card.Body>
-      <Card.Body>
-      
-        We specialize in helping small business owners, especially those who are underrepresented, with the ability to list and sell items online. This allows business owners to reach a wider audience while still remaining in control of their own business.
-     
-      </Card.Body>
-    </div>
-    </Card>
-  </Container>
-  </div> */}
+export default Home;
